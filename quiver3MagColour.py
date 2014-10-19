ebdaa0ec4691750bfd8070799cdaf251f94c8cb4
J3TTBlack88 <jasoncrewebrown@gmail.com>
2014-10-20 00:43:27 +0200

    Update quiver3MagColour.py

--- quiver3MagColour.py
+++ quiver3MagColour.py
@@ -6,6 +6,7 @@ import math as m
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
+# Data is a list of lists with the entries [x,y,z,theta,beta,gamma,velocity]. Omega is angle to x-axis etc.
 data=[[0.5,0.1,0.04,0,90,90,0.15],[0.75,0.1,0.04,0,90,90,0.08],[0.95,0.1,0.04,5,85,30,0.02],
 [0.5,0.285,0.04,0,90,90,0.12],[0.75,0.285,0.04,0,90,90,0.06],[0.95,0.285,0.04,0,90,30,0.02],
 [0.5,0.52,0.04,0,90,90,0.1],[0.75,0.52,0.04,0,90,90,0.04],[0.95,0.52,0.04,-5,85,30,0.02],
@@ -30,11 +31,15 @@ data=[[0.5,0.1,0.04,0,90,90,0.15],[0.75,0.1,0.04,0,90,90,0.08],[0.95,0.1,0.04,5,
 [0.85,0.285,0.7,0,90,60,0.06],[0.85,0.52,0.7,-4,86,60,0.05],[0.55,0.1,0.7,1,89,70,0.07],
 [0.55,0.285,0.7,0,90,70,0.05],[0.55,0.52,0.7,-1,89,70,0.04]]
 
+#This colour map suits my needs, it's a cubic progression according to velocity magnitude so that lower steps 
+#are differentiated more. My Plot is 0=Green100% and 0.7=Red100%
 colors=["#FF0000","#EC1200","#DA2400","#C83600","#B64800",
 "#A35B00","#916D00","#7F7F00","#6D9100","#5BA300",
 "#48B600","#36C800","#24DA00","#12EC00","#00FF00"]
 
+#Quiver3 doesn't allow a list type colour map, hence the points are plotted individually:
 for i in range(0,len(data)):
+    #The nest few lines are my 4am direction considerations
     xdir=1
     ydir=1
     zdir=1
@@ -48,6 +53,7 @@ for i in range(0,len(data)):
     v=data[i][6]*np.cos(np.radians(data[i][4]))*ydir
     w=data[i][6]*np.cos(np.radians(data[i][5]))*zdir
 
+    #Assignment of colours according to magnitude, according to cubic steps discussed above
     if data[i][6] >= 0.59 and data[i][6] < 0.7 : 
      tone=colors[ 0 ]
     elif data[i][6] >= 0.5 and data[i][6] < 0.59 : 
@@ -79,8 +85,10 @@ for i in range(0,len(data)):
     elif data[i][6] >= 0 and data[i][6] < 0.01 : 
      tone=colors[ 14 ]
     else:
+        #Outliers plotted in Blue100% for defference
         tone='#0000FF'
     
+    #Actual plotting of coloured arrows
     ax.quiver(data[i][0], data[i][1], data[i][2], u, v, w, length=0.1, color=tone)
 
 #ax.colorbar() TODO - Not supported
